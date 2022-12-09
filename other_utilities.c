9fc41f92f9cc884aa115f7e49e9e4faf865171a4
FabioH7 <fhysollari4@gmail.com>
2022-12-09 02:52:59 -0800

    fix execute function

--- other_utilities.c
+++ other_utilities.c
@@ -68,10 +68,10 @@ char *command_path(char *cmd)
 	struct stat buf;
 	int i = 0;
 
-	new_path = malloc(sizeof(char) * 100);
 	if (path == NULL)
 		if (stat(cmd, &buf) == 0)
 			return (_strdup(cmd));
+	new_path = malloc(sizeof(char) * 100);
 	while (token != NULL)
 	{
 		path_array[i] = token;
