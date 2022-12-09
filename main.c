efabcafab6da2ab514492c019cb6f8624d83bea7
FabioH7 <fhysollari4@gmail.com>
2022-12-09 08:41:47 -0800

    tests

--- main.c
+++ main.c
@@ -49,7 +49,7 @@ int main(int __attribute__((unused)) argc, char *argv[])
 	char *line = NULL;
 	size_t buff_size = 0;
 	ssize_t characters = 0;
-	int read_value, flag = 0;
+	int read_value;
 
 	name = argv[0];
 
