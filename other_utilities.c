9a047f6ab6c7d05d5cc008377d3e778ff1c6eac9
FabioH7 <fhysollari4@gmail.com>
2022-12-09 04:22:38 -0800

    fixing exit

--- other_utilities.c
+++ other_utilities.c
@@ -68,7 +68,7 @@ char *command_path(char *cmd)
 	struct stat buf;
 	int i = 0;
 
-	if (_getenv("PATH")[0] == ':')
+	if (_getenv("PATH")[0] == '\0')
 		if (stat(cmd, &buf) == 0)
 			return (_strdup(cmd));
 	new_path = malloc(sizeof(char) * 100);
